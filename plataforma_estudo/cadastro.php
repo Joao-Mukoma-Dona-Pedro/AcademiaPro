9b3b5cb16d0f71c1576bd5f66422f485b063692c
Joao-Mukoma-Dona-Pedro <mukomamuamba@gmail.com>
2026-05-01 15:33:41 +0100

    Update cadastro.php

--- plataforma_estudo/cadastro.php
+++ plataforma_estudo/cadastro.php
@@ -29,7 +29,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Cadastro - AcademiaPro</title>
-  <link rel="stylesheet" href="style.css">  <!-- caminho corrigido -->
+  <link rel="stylesheet" href="../estilo/stilo.css">  <!-- caminho corrigido -->
 </head>
 <body>
 
